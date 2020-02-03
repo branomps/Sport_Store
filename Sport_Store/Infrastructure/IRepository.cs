a8da9fdc823bef3afa7cbd8274b6935620b4837f
BranislavMacosinec <branomps@gmail.com>
2020-02-03 13:44:21 +0000

    Add irepository

---
+++ Sport_Store/Infrastructure/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Store.Infrastructure
+{
+    interface IRepository
+    {
+        public interface IRepository
+        {
+            IEnumerable<Product> Products { get; }
+            void AddProduct(Product product);
+        }
+    }
+}
