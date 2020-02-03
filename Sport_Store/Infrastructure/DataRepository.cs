6b549ad16ec47f43d655f9933d57026fb626c08d
BranislavMacosinec <branomps@gmail.com>
2020-02-03 14:06:01 +0000

    Add IRepository and DataRepository

---
+++ Sport_Store/Infrastructure/DataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sport_Store.Models;
+
+namespace Sport_Store.Infrastructure
+{
+    public class DataRepository : IRepository
+    {
+        private List<Product> data = new List<Product>();
+        public IEnumerable<Product> Products => data;
+        public void AddProduct (Product product)
+        {
+            this.data.Add(product);
+        }
+    }
+}
--- Sport_Store/Infrastructure/IRepository.cs
+++ Sport_Store/Infrastructure/IRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Sport_Store.Models;
 
 namespace Sport_Store.Infrastructure
 {
