7704cd6bd55c7a9b033aaaf280eae8027b570f2f
BranislavMacosinec <branomps@gmail.com>
2020-02-04 13:11:04 +0000

    Configure database provider

--- Sport_Store/Startup.cs
+++ Sport_Store/Startup.cs
@@ -1,26 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Sport_Store.Models;
 using Sport_Store.Infrastructure;
-using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 namespace Sport_Store
 {
     public class Startup
     {
+        public Startup(IConfiguration config) => Configuration = config;
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false);
-            services.AddSingleton<IRepository, DataRepository>();
+            services.AddTransient<IRepository, DataRepository>();
+           // string conString = Configuration["ConnectionString: DefaultConnection"];
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
